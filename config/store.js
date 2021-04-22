6afb321dc17e4d4dbcdb180987bb738a865a806c
ricardo <ricardo.ortiz@trade.ec>
2021-04-22 18:18:44 -0500

    log url in store configs

--- config/store.js
+++ config/store.js
@@ -1,5 +1,5 @@
 const clientUrl = process.env.CLIENT_URL || 'http://localhost:3001';
-
+console.log('----cliente URL', clientUrl);
 // config used by store client side only
 module.exports = {
 	// store UI language
