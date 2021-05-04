9fed153cf375d8e67beef428c67c17bba9efc04b
ricardo <ricardo.ortiz@trade.ec>
2021-05-03 22:45:09 -0500

    Added Json stringify

--- webpack.config.store.js
+++ webpack.config.store.js
@@ -128,8 +128,12 @@ module.exports = {
 			entryOnly: false
 		}),
 		new webpack.DefinePlugin({
-			'process.env.REACT_APP_CLIENT_URL': process.env.REACT_APP_CLIENT_URL,
-			'process.env.REACT_APP_SERVER_URL': process.env.REACT_APP_SERVER_URL
+			'process.env.REACT_APP_CLIENT_URL': JSON.stringify(
+				process.env.REACT_APP_CLIENT_URL
+			),
+			'process.env.REACT_APP_SERVER_URL': JSON.stringify(
+				process.env.REACT_APP_SERVER_URL
+			)
 		})
 	],
 
